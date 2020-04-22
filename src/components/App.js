cebef7f877ecce2daf1a23c9077432e86f159fa6
Hyunsu Kim <hyunsu.j.kim@gmail.com>
2020-04-22 18:51:22 -0500

    used arrow function

--- src/components/App.js
+++ src/components/App.js
@@ -5,21 +5,17 @@ import Note from "./Note";
 import notes from "../notes";
 import { createNotEmittedStatement } from "typescript";
 
-function createNotes(noteItem) {
-  return (
-    <Note
-      key={noteItem.key}
-      title={noteItem.title}
-      content={noteItem.content}
-    />
-  );
-}
-
 function App() {
   return (
     <div>
       <Header />
-      {notes.map(createNotes)}
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
       <Footer />
     </div>
   );
